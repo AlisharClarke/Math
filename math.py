58d5f90482efc746b0b98bcea95b3b0749b3a20f
AlisharClarke <52299711+AlisharClarke@users.noreply.github.com>
2019-08-02 09:47:31 -0700

    Add files via upload

---
+++ math.py
@@ -0,0 +1,16 @@
+import math
+math1 = math.copysign(-7,9)
+#takes the absolute value of the x
+print(math1)
+math2 = math.fabs(-5)
+#takes absolute value
+print(math2)
+math3 = math.factorial(4)
+#4! = 1 x 2 x 3 x 4
+print(math3)
+math4 = math.floor(3.9)
+#Rounds all the way down
+print(math4)
+math5 = math.ceil(3.1)
+# Rounds all the way up
+print(math5)
